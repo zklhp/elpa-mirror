c41616651aceaa9fdc223a35c4efb01ec3d1b064
codefalling <code.falling@gmail.com>
2016-05-05 16:17:52 +0800

    fix permission denied

--- travis.sh
+++ travis.sh
@@ -2,11 +2,13 @@ git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
 mkdir ~/.ssh
 echo -e $SSH_KEY > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-chmod 600 ~/.ssh
 
 ssh-keyscan git.coding.net >> key
 ssh-keygen -lf key >> ~/.ssh/known_hosts
+
+chmod 600 ~/.ssh/id_rsa
+chmod 600 ~/.ssh
+
 
 git clone git@git.coding.net:codefalling/melpa-mirror.git packages
 npm install
