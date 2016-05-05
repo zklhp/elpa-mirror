66285357475843311bd24bf4a467b6c9a86044dd
codefalling <code.falling@gmail.com>
2016-05-05 16:38:57 +0800

    ..

--- travis.sh
+++ travis.sh
@@ -2,15 +2,16 @@ git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
 echo -e $SSH_KEY > ~/.ssh/id_rsa
 
-chmod 600 ~/.ssh/id_rsa
-chmod 600 ~/.ssh
+cp ssh_config ~/.ssh/ssh_config
+# chmod 600 ~/.ssh/id_rsa
+# chmod 600 ~/.ssh
 
-GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git clone git@git.coding.net:codefalling/melpa-mirror.git packages
+git clone git@git.coding.net:codefalling/melpa-mirror.git packages
 npm install
 node index.js
 cd packages
 git add .
 git commit -m "update"
 
-GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push origin master:master --quiet
+git push origin master:master --quiet
 
