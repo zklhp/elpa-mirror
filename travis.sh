d6108c647ddd69d166a02b1f4c47bbc01e1ca1ca
codefalling <code.falling@gmail.com>
2016-05-05 16:15:28 +0800

    ..

--- travis.sh
+++ travis.sh
@@ -1,11 +1,13 @@
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
 mkdir ~/.ssh
-cat codingKey >> ~/.ssh/known_hosts
 echo -e $SSH_KEY > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 chmod 600 ~/.ssh
-export GIT_TERMINAL_PROMPT=0
+
+ssh-keyscan git.coding.net >> key
+ssh-keygen -lf key >> ~/.ssh/known_hosts
+
 git clone git@git.coding.net:codefalling/melpa-mirror.git packages
 npm install
 node index.js
