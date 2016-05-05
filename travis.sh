224a56ca473c87fd3ea312e2ff432b3cb7dec753
codefalling <code.falling@gmail.com>
2016-05-05 15:30:06 +0800

    Fix permission issue

--- travis.sh
+++ travis.sh
@@ -3,9 +3,9 @@ git config --global user.name "$GIT_NAME"
 rm -r -f ~/.ssh
 mkdir ~/.ssh
 echo -e $SSH_KEY > ~/.ssh/id_rsa
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config # Disable strict host checking for git clone
 chmod 600 ~/.ssh/id_rsa
 chmod 600 ~/.ssh
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config # Disable strict host checking for git clone
 git clone git@git.coding.net:codefalling/melpa-mirror.git packages
 npm install
 node index.js
