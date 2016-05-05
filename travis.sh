97ef8925b6791f927322d09328f33da8f04ed126
codefalling <code.falling@gmail.com>
2016-05-05 16:23:46 +0800

    remove chmod

--- travis.sh
+++ travis.sh
@@ -1,13 +1,11 @@
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
-mkdir ~/.ssh
 echo -e $SSH_KEY > ~/.ssh/id_rsa
 
 ssh-keyscan git.coding.net >> key
 ssh-keygen -lf key >> ~/.ssh/known_hosts
 
-chmod 600 ~/.ssh/id_rsa
-chmod 600 ~/.ssh
+# chmod 600 ~/.ssh/id_rsa
 
 
 git clone git@git.coding.net:codefalling/melpa-mirror.git packages
