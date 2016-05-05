6c2377ab0f0b13e4b903f02e2aa8147e582c4003
codefalling <code.falling@gmail.com>
2016-05-05 15:38:42 +0800

    trust coding.net

--- travis.sh
+++ travis.sh
@@ -3,9 +3,10 @@ git config --global user.name "$GIT_NAME"
 rm -r -f ~/.ssh
 mkdir ~/.ssh
 echo -e $SSH_KEY > ~/.ssh/id_rsa
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config # Disable strict host checking for git clone
+ssh-keyscan git.coding.net >> ~/.ssh/known_hosts
 chmod 600 ~/.ssh/id_rsa
 chmod 600 ~/.ssh
+export GIT_TERMINAL_PROMPT=0
 git clone git@git.coding.net:codefalling/melpa-mirror.git packages
 npm install
 node index.js
