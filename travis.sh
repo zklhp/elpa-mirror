673320fa7835e841aa86163e46b9af7090b3c561
codefalling <code.falling@gmail.com>
2016-05-05 19:43:46 +0800

    move to oschina

--- travis.sh
+++ travis.sh
@@ -2,8 +2,8 @@ git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
 echo -e $SSH_KEY > ~/.ssh/id_rsa
 
-echo "Host git.coding.net" >> ~/.ssh/config
-echo "   Hostname git.coding.net" >> ~/.ssh/config
+echo "Host git.oschina.net" >> ~/.ssh/config
+echo "   Hostname git.oschina.net" >> ~/.ssh/config
 echo "   StrictHostKeyChecking no" >> ~/.ssh/config
 echo "   CheckHostIP no" >> ~/.ssh/config
 echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
@@ -12,7 +12,7 @@ chmod 600 ~/.ssh/id_rsa
 # chmod 600 ~/.ssh
 
 # cat ~/.ssh/config
-git clone git@git.coding.net:codefalling/elpa.git packages
+git clone git@git.oschina.net:EmacsChina/elpa.git packages
 npm install
 node index.js
 cd packages
