dd7730d90f6bc4d65bd7d6fac4fb3dd61fe62943
codefalling <code.falling@gmail.com>
2016-05-05 15:18:25 +0800

    add script

---
+++ travis.sh
@@ -0,0 +1,14 @@
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_NAME"
+mkdir ~/.ssh
+echo -e $SSH_KEY > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+chmod 600 ~/.ssh
+git clone git@git.coding.net:codefalling/melpa-mirror.git packages
+npm install
+node index.js
+cd packages
+git add .
+git commit -m "update"
+git push origin master:master --quiet
+
