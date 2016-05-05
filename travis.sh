f96eea38d24a3ce5b635d160e33dd0c080a036bb
codefalling <code.falling@gmail.com>
2016-05-05 16:53:07 +0800

    remove chmod

--- travis.sh
+++ travis.sh
@@ -9,7 +9,7 @@ echo "   CheckHostIP no" >> ~/.ssh/config
 echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
 
 chmod 600 ~/.ssh/id_rsa
-chmod 600 ~/.ssh
+# chmod 600 ~/.ssh
 
 # cat ~/.ssh/config
 git clone git@git.coding.net:codefalling/melpa-mirror.git packages
