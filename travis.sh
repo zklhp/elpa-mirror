5faa089c6d0bab820283667eb29962e5e26af866
codefalling <code.falling@gmail.com>
2016-05-06 17:31:07 +0800

    try gh-pages

--- travis.sh
+++ travis.sh
@@ -2,8 +2,8 @@ git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
 echo -e $SSH_KEY > ~/.ssh/id_rsa
 
-echo "Host git.oschina.net" >> ~/.ssh/config
-echo "   Hostname git.oschina.net" >> ~/.ssh/config
+echo "Host github.com" >> ~/.ssh/config
+echo "   Hostname github.com" >> ~/.ssh/config
 echo "   StrictHostKeyChecking no" >> ~/.ssh/config
 echo "   CheckHostIP no" >> ~/.ssh/config
 echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
@@ -12,7 +12,7 @@ chmod 600 ~/.ssh/id_rsa
 # chmod 600 ~/.ssh
 
 # cat ~/.ssh/config
-git clone git@git.oschina.net:EmacsChina/elpa.git packages --depth=1
+git clone git@github.com:CodeFalling/elpa-mirror.git packages -b gh-pages --depth=1
 
 # remove old files
 rm -r -f packages/gnu
@@ -25,5 +25,5 @@ cd packages
 git add .
 git commit -m "update"
 
-git push origin master:master
+git push origin gh-pages:gh-pages
 
