048bcf723d8290c6bf4571bc550b79a281708bd0
codefalling <code.falling@gmail.com>
2016-05-06 17:53:39 +0800

    coding pages

--- travis.sh
+++ travis.sh
@@ -2,8 +2,8 @@ git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
 echo -e $SSH_KEY > ~/.ssh/id_rsa
 
-echo "Host github.com" >> ~/.ssh/config
-echo "   Hostname github.com" >> ~/.ssh/config
+echo "Host git.coding.net" >> ~/.ssh/config
+echo "   Hostname git.coding.net" >> ~/.ssh/config
 echo "   StrictHostKeyChecking no" >> ~/.ssh/config
 echo "   CheckHostIP no" >> ~/.ssh/config
 echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
@@ -12,7 +12,7 @@ chmod 600 ~/.ssh/id_rsa
 # chmod 600 ~/.ssh
 
 # cat ~/.ssh/config
-git clone git@github.com:CodeFalling/elpa-mirror.git packages -b gh-pages --depth=1
+git clone git@git.coding.net:codefalling/elpa.git packages -b coding-pages --depth=1
 
 # remove old files
 rm -r -f packages/gnu
@@ -25,5 +25,5 @@ cd packages
 git add .
 git commit -m "update"
 
-git push origin gh-pages:gh-pages
+git push origin coding-pages:coding-pages
 
